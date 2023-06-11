9cbd315ad487efe0b847db399695a76588a13f90
Laura Heimann <hello@laura.media>
2023-06-11 13:59:25 +0200

    Release (#28)

--- UserInterface/src/api/api.js
+++ UserInterface/src/api/api.js
@@ -1,9 +1,12 @@
+export const SUPPORTED_VERSION = 1;
+export const FEATURED_PLAYLIST_ID = 144;
+
 async function getData(endpoint) {
     const res = await fetch('https://spinsha.re/api/' + endpoint);
     const body = await res.json();
 
-    if(body.version !== 1) {
-        console.error("API version mismatch");
+    if(body.version !== SUPPORTED_VERSION) {
+        console.error(`[API] Version Mismatch (Got ${body.version}, needed ${SUPPORTED_VERSION})`);
         return null;
     }
 
@@ -16,21 +19,26 @@ async function getData(endpoint) {
 }
 
 export async function getPromos() {
+    console.log("[API] getPromos");
     return await getData('promos') ?? [];
 }
 
 export async function getChart(chartId) {
+    console.log("[API] getChart " + chartId);
     return await getData('song/' + chartId) ?? null;
 }
 
 export async function getPlaylist(playlistId) {
+    console.log("[API] getPlaylist " + playlistId);
     return await getData('playlist/' + playlistId) ?? null;
 }
 
 export async function getChartReviews(chartId) {
+    console.log("[API] getChartReviews " + chartId);
     return await getData('song/' + chartId + '/reviews') ?? [];
 }
 
 export async function getChartSpinPlays(chartId) {
+    console.log("[API] getChartSpinPlays " + chartId);
     return await getData('song/' + chartId + '/spinplays') ?? [];
 }
