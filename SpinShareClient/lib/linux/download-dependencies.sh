cae807eedf8a7cf6bee88c0347078eddd863544c
Laura Sofia Heimann <hello@laura.media>
2024-01-17 00:52:21 +0100

    added linux dependencies

--- SpinShareClient/lib/linux/download-dependencies.sh
+++ SpinShareClient/lib/linux/download-dependencies.sh
@@ -4,13 +4,13 @@
 TARGET_DIR="."
 
 # Get the shared library dependencies using ldd
-LIBS=$(ldd /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.0.so.37 | grep "=>" | awk '{print $3}' | sort -u)
+LIBS=$(ldd /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.1.so.0 | grep "=>" | awk '{print $3}' | sort -u)
 
 # Create the target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 
 # Copy the main library
-cp /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.0.so.37 "$TARGET_DIR"
+cp /usr/lib/x86_64-linux-gnu/libwebkit2gtk-4.1.so.0 "$TARGET_DIR"
 
 # Copy the library dependencies
 for lib in $LIBS; do
