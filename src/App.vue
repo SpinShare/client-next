bedf7705deb08bedbb53e4bc3d5b054b8181daf2
Laura Sofia Heimann <hello@laura.media>
2025-03-07 09:47:21 +0100

    Setup vue

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
+
+<template>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+</template>
