b20a290878b343a7dbff12510b1188dcf2521fec
Laura Sofia Heimann <hello@laura.media>
2025-04-14 17:26:31 +0200

    Add Sentry SourceMaps to main

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,4 +1,14 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    sourcemap: true
+  },
+
+  plugins: [sentryVitePlugin({
+    org: "spinshare",
+    project: "client-next"
+  })]
+});
