7537d6ee5900a90b043cc7554a477c05744c37b8
Laura Sofia Heimann <hello@laura.media>
2025-04-24 16:57:22 +0200

    Rename getUser to getUserDetail

--- src/main/api/user.js
+++ src/main/api/user.js
@@ -1,7 +1,7 @@
 import { ipcMain } from 'electron';
 
 export function setupUserApiHandlers(apiClient) {
-    ipcMain.handle('get-user', async (event, userId) => {
+    ipcMain.handle('get-user-detail', async (event, userId) => {
         try {
             return await apiClient.getUserDetail(userId);
         } catch (e) {
