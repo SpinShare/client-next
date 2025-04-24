db734156ee8e0f96ebda65502ea07baf308d8fef
Laura Sofia Heimann <hello@laura.media>
2025-04-24 17:02:54 +0200

    Add uploader item to spinplay item

--- src/renderer/components/SpinPlayItem.vue
+++ src/renderer/components/SpinPlayItem.vue
@@ -6,15 +6,24 @@
         <div
             class="thumbnail"
             :style="`background-image: url('${videoThumbnail}')`"
-        ></div>
-
-        <div class="user">
-            {{ user }}
+        >
+            <div class="overlay">
+                <Remixicon
+                    icon="play"
+                    size="5xl"
+                    filled
+                />
+            </div>
         </div>
+
+        <UserItem v-bind="user" />
     </a>
 </template>
 
 <script setup>
+import UserItem from '@/components/UserItem.vue';
+import Remixicon from '@/components/Remixicon.vue';
+
 const props = defineProps({
     id: {
         type: Number,
@@ -40,11 +49,18 @@ const props = defineProps({
     @apply bg-base-900 rounded-md overflow-hidden transition-all cursor-pointer;
 
     & .thumbnail {
-        @apply bg-cover bg-center aspect-video w-full;
-    }
+        @apply bg-cover bg-center aspect-video w-full relative;
 
-    &:hover {
-        @apply bg-base-800;
+        & .overlay {
+            @apply transition-all flex items-center justify-center absolute inset-0 opacity-0;
+            background: rgba(0, 0, 0, 0.6);
+        }
+
+        &:hover {
+            & .overlay {
+                @apply opacity-100;
+            }
+        }
     }
 }
 </style>
