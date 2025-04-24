4931ce73861375ee61421e3c421e3a15db3e15f7
Laura Sofia Heimann <hello@laura.media>
2025-04-24 20:41:12 +0200

    Add user charts

--- src/renderer/views/User/ViewUserCharts.vue
+++ src/renderer/views/User/ViewUserCharts.vue
@@ -1,7 +1,56 @@
 <template>
-    <h1>User Charts</h1>
+    <section class="page-user-charts">
+        <div
+            class="no-charts"
+            v-if="charts.length === 0"
+        >
+            <Remixicon
+                icon="award"
+                size="3xl"
+                filled
+            />
+            <span>No charts yet.</span>
+        </div>
+        <ChartGrid>
+            <ChartItem
+                v-for="chart in charts"
+                :key="chart.id"
+                v-bind="chart"
+            />
+        </ChartGrid>
+    </section>
 </template>
 
-<script setup></script>
+<script setup>
+import Remixicon from '@/components/Remixicon.vue';
+import ChartGrid from '@/components/ChartGrid.vue';
+import ChartItem from '@/components/ChartItem.vue';
+import { onMounted, ref, inject } from 'vue';
+import { useRoute } from 'vue-router';
 
-<style scoped></style>
+const api = inject('api');
+const route = useRoute();
+const userId = route.params.userId;
+const charts = ref([]);
+
+const props = defineProps({
+    user: {
+        type: Object,
+        default: null,
+    },
+});
+
+onMounted(async () => {
+    charts.value = await api.getUserCharts(userId);
+});
+</script>
+
+<style scoped>
+.page-user-charts {
+    @apply p-10;
+
+    & .no-charts {
+        @apply flex flex-col items-center gap-1 py-5 text-base-400 border border-base-800 rounded-md;
+    }
+}
+</style>
