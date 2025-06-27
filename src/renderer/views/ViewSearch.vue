a1288995b5e682d28cad599c5c23b037ccc22467
Laura Sofia Heimann <hello@laura.media>
2025-06-27 19:46:20 +0200

    Implement search results and initial UI for chart search filters

--- src/renderer/views/ViewSearch.vue
+++ src/renderer/views/ViewSearch.vue
@@ -7,13 +7,36 @@
         </template>
         <template v-else>
             <template v-if="searchType === 'charts'">
-                CHART RESULT {{ results }}
+                <section class="chart-results">
+                    <div class="filters">
+                        <button class="button">
+                            <Remixicon icon="shapes" :filled="true" />
+                            <span>E, N, H, EX, XD</span>
+                        </button>
+                        <button class="button">
+                            <Remixicon icon="expand-width" :filled="true" />
+                            <span>0 — 99</span>
+                        </button>
+                        <button class="button">
+                            <Remixicon icon="checkbox-circle" :filled="true" />
+                            <span>Explicit</span>
+                        </button>
+                    </div>
+
+                    <ChartGrid>
+                        <ChartItem v-for="chart in results" :key="chart.id" v-bind="chart" />
+                    </ChartGrid>
+                </section>
             </template>
             <template v-if="searchType === 'playlists'">
-                PLAYLIST RESULT {{ results }}
+                <PlaylistGrid class="p-10">
+                    <PlaylistItem v-for="playlist in results" :key="playlist.id" v-bind="playlist" />
+                </PlaylistGrid>
             </template>
             <template v-if="searchType === 'users'">
-                USERS RESULT {{ results }}
+                <section class="user-grid">
+                    <UserItem v-for="user in results" :key="user.id" v-bind="user" />
+                </section>
             </template>
         </template>
     </LayoutBase>
@@ -22,8 +45,14 @@
 <script setup>
 import LayoutBase from '@/layouts/LayoutBase.vue';
 import {useRoute} from "vue-router";
-import {computed, inject, onMounted, ref, watch} from "vue";
+import {computed, inject, nextTick, onMounted, ref, watch} from "vue";
 import Loader from "@/components/Loader.vue";
+import UserItem from "@/components/UserItem.vue";
+import PlaylistGrid from "@/components/Playlists/PlaylistGrid.vue";
+import PlaylistItem from "@/components/Playlists/PlaylistItem.vue";
+import ChartGrid from "@/components/Charts/ChartGrid.vue";
+import ChartItem from "@/components/Charts/ChartItem.vue";
+import Remixicon from "@/components/Remixicon.vue";
 
 const api = inject('api');
 const route = useRoute();
@@ -37,19 +66,49 @@ async function loadResults() {
     isLoading.value = true;
     results.value = [];
 
-    //api
+    switch(searchType.value) {
+        case 'charts':
+            results.value = await api.searchCharts(searchQuery.value, {});
+            break;
+        case 'playlists':
+            results.value = await api.searchPlaylists(searchQuery.value);
+            break;
+        case 'users':
+            results.value = await api.searchUsers(searchQuery.value);
+            break;
+    }
 
-    // ...
-
-    //isLoading.value = false;
+    isLoading.value = false;
 }
 
 onMounted(() => {
     loadResults();
 });
-watch(() => [searchType, searchQuery], () => {
+watch(() => [searchType.value, searchQuery.value], () => {
     loadResults();
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.chart-results {
+    @apply p-10 flex flex-col gap-5;
+
+    & .filters {
+        @apply flex flex-wrap gap-2 items-center;
+    }
+}
+
+.user-grid {
+    @apply flex flex-col gap-3 p-10;
+}
+@media screen and (min-width: 900px) {
+    .user-grid {
+        @apply grid grid-cols-2;
+    }
+}
+@media screen and (min-width: 1300px) {
+    .user-grid {
+        @apply grid grid-cols-3;
+    }
+}
+</style>
