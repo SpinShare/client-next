0d9d16f4317707d96683e8c53a1c259109d5218f
Laura Sofia Heimann <hello@laura.media>
2025-06-27 18:57:06 +0200

    Setup header for search

--- src/renderer/components/Header/Header.vue
+++ src/renderer/components/Header/Header.vue
@@ -18,9 +18,19 @@
         </div>
         <div class="search">
             <div class="search-box">
+                <select
+                    v-interactable
+                    v-model="searchType"
+                >
+                    <option value="charts">Charts</option>
+                    <option value="playlists">Playlists</option>
+                    <option value="users">Users</option>
+                </select>
                 <input
                     v-interactable
                     type="search"
+                    v-model="searchQuery"
+                    @keyup.enter="handleSearch"
                     placeholder="Search for a chart, charter, or playlist..."
                 />
             </div>
@@ -32,16 +42,24 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import {useRoute, useRouter} from 'vue-router';
 import SpinShareLogo from '@/assets/images/logo_full.svg';
 import Remixicon from '@/components/Remixicon.vue';
-import { computed } from 'vue';
+import {computed, ref} from 'vue';
 import AuthArea from '@/components/Header/AuthArea.vue';
 
 const router = useRouter();
+const route = useRoute();
+
+const searchType = ref(route.params.type || 'charts');
+const searchQuery = ref(route.params.query || '');
 
 function handleBack() {
     router.go(-1);
+}
+
+function handleSearch() {
+    router.push(`/discover/search/${searchType.value}/${searchQuery.value}`);
 }
 
 const hasHistory = computed(() => {
@@ -65,10 +83,22 @@ header.main {
         @apply flex items-center justify-center;
 
         & .search-box {
-            @apply h-[40px] flex overflow-hidden rounded-md relative min-w-[200px] w-full max-w-[500px] border border-base-800;
+            @apply h-[40px] flex gap-0 rounded-md relative min-w-[200px] w-full max-w-[500px] border border-base-800;
 
+            & select {
+                @apply px-2 border-r border-base-800 appearance-none pr-8;
+
+                & option {
+                    @apply bg-base-900;
+                }
+
+                background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='rgba(255,255,255,1)'%3E%3Cpath d='M12 15.0006L7.75732 10.758L9.17154 9.34375L12 12.1722L14.8284 9.34375L16.2426 10.758L12 15.0006Z'%3E%3C/path%3E%3C/svg%3E");
+                background-repeat: no-repeat;
+                background-position: right 4px top 60%;
+                background-size: 24px 24px;
+            }
             & input {
-                @apply grow px-4 absolute inset-0 font-sans bg-none transition-all;
+                @apply grow px-4 font-sans bg-none transition-all;
 
                 &:hover {
                     @apply bg-base-900;
--- src/renderer/views/ViewSearch.vue
+++ src/renderer/views/ViewSearch.vue
@@ -1,11 +1,55 @@
 <template>
     <LayoutBase>
-        <h1>Search</h1>
+        <template v-if="isLoading">
+            <section class="section-center">
+                <Loader />
+            </section>
+        </template>
+        <template v-else>
+            <template v-if="searchType === 'charts'">
+                CHART RESULT {{ results }}
+            </template>
+            <template v-if="searchType === 'playlists'">
+                PLAYLIST RESULT {{ results }}
+            </template>
+            <template v-if="searchType === 'users'">
+                USERS RESULT {{ results }}
+            </template>
+        </template>
     </LayoutBase>
 </template>
 
 <script setup>
 import LayoutBase from '@/layouts/LayoutBase.vue';
+import {useRoute} from "vue-router";
+import {computed, inject, onMounted, ref, watch} from "vue";
+import Loader from "@/components/Loader.vue";
+
+const api = inject('api');
+const route = useRoute();
+const searchType = computed(() => route.params.type);
+const searchQuery = computed(() => route.params.query);
+
+const isLoading = ref(false);
+const results = ref([]);
+
+async function loadResults() {
+    isLoading.value = true;
+    results.value = [];
+
+    //api
+
+    // ...
+
+    //isLoading.value = false;
+}
+
+onMounted(() => {
+    loadResults();
+});
+watch(() => [searchType, searchQuery], () => {
+    loadResults();
+});
 </script>
 
 <style scoped></style>
