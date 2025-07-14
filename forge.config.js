47a032de29aff8b07c42a46eabfc9cea471d21b5
Laura Sofia Heimann <hello@laura.media>
2025-07-14 18:57:01 +0200

    Fix deep link registration

--- forge.config.js
+++ forge.config.js
@@ -6,58 +6,21 @@ module.exports = {
         asar: true,
         executableName: 'spinshare-client-next',
         icon: './src/renderer/assets/images/icon',
-        productName: "SpinShare",
+        productName: 'SpinShare',
+        protocols: [
+            {
+                name: "SpinShare",
+                schemes: ["spinshare"]
+            }
+        ]
     },
     rebuildConfig: {},
     makers: [
         {
             name: '@electron-forge/maker-squirrel',
             config: {
-                // Register the spinshare:// protocol during installation
                 setupExe: 'SpinShare-Setup.exe',
                 setupIcon: './src/renderer/assets/images/icon.ico',
-                registryItems: [
-                    {
-                        name: 'spinshare',
-                        path: [
-                            'SOFTWARE',
-                            'Classes',
-                            'spinshare'
-                        ],
-                        value: 'URL:SpinShare Protocol'
-                    },
-                    {
-                        name: 'URL Protocol',
-                        path: [
-                            'SOFTWARE',
-                            'Classes',
-                            'spinshare'
-                        ],
-                        value: ''
-                    },
-                    {
-                        name: 'DefaultIcon',
-                        path: [
-                            'SOFTWARE',
-                            'Classes',
-                            'spinshare',
-                            'DefaultIcon'
-                        ],
-                        value: '"@APPPATH@,1"'
-                    },
-                    {
-                        name: '',
-                        path: [
-                            'SOFTWARE',
-                            'Classes',
-                            'spinshare',
-                            'shell',
-                            'open',
-                            'command'
-                        ],
-                        value: '"@APPPATH@" "%1"'
-                    }
-                ]
             },
         },
         {
@@ -65,29 +28,9 @@ module.exports = {
             platforms: ['linux'],
             config: {
                 executableName: 'spinshare-client-next',
-            }
+                mimeType: ["x-scheme-handler/spinshare"]
+            },
         },
-        /*
-        {
-            name: '@electron-forge/maker-deb',
-            platforms: ['linux'],
-            config: {
-                options: {
-                    icon: './src/renderer/assets/images/icon.png',
-                    id: 're.spinsha.client',
-                    productName: 'SpinShare',
-                    genericName: 'SpinShare',
-                    description: 'A modern cross-platform desktop client for SpinShare',
-                    branch: 'dev',
-                    categories: ['Game'],
-                    // Add protocol handler registration for Debian
-                    mimeType: ['x-scheme-handler/spinshare'],
-                    maintainer: 'SpinShare',
-                    homepage: 'https://spinsha.re/',
-                },
-                executableName: 'spinshare-client-next',
-            }
-        }, */
     ],
     plugins: [
         {
