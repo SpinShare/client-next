ee83c066a6c7bdc7c28ee0a6c8504c01906642f9
Laura Sofia Heimann <hello@laura.media>
2025-07-23 14:53:45 +0200

    Add Dutch translations

--- src/renderer/views/ViewSettings.vue
+++ src/renderer/views/ViewSettings.vue
@@ -50,7 +50,7 @@
                         <option value="de">German</option>
                         <option value="fr">French</option>
                         <option value="es">Spanish</option>
-                        <!-- <option value="nl">Dutch</option> -->
+                        <option value="nl">Dutch</option>
                         <option value="speen">SPEEN</option>
                     </select>
                 </SettingsItem>
