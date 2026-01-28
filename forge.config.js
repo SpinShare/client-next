97306bec08b0d1ce791344c05f02c82fc14b0292
alpinesystem <ellite@ellite.dev>
2026-01-28 01:45:06 +0100

    Add AppImage maker

--- forge.config.js
+++ forge.config.js
@@ -31,16 +31,27 @@ module.exports = {
                 mimeType: ['x-scheme-handler/spinshare'],
             },
         },
+        {
+            name: '@reforged/maker-appimage',
+            platforms: ['linux'],
+            config: {
+                options: {
+                    bin: 'spinshare-client-next',
+                    name: 'SpinShare',
+                    productName: 'SpinShare',
+                    genericName: 'SpinShare Client',
+                    categories: ['Audio', 'Game'],
+                    icon: './src/renderer/assets/images/icon.png',
+                },
+            },
+        },
     ],
     plugins: [
         {
             name: '@electron-forge/plugin-vite',
             config: {
-                // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
-                // If you are familiar with Vite configuration, it will look really familiar.
                 build: [
                     {
-                        // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
                         entry: 'src/main.js',
                         config: 'vite.main.config.mjs',
                         target: 'main',
@@ -59,8 +70,6 @@ module.exports = {
                 ],
             },
         },
-        // Fuses are used to enable/disable various Electron functionality
-        // at package time, before code signing the application
         new FusesPlugin({
             version: FuseVersion.V1,
             [FuseV1Options.RunAsNode]: false,
