4fcd33836c3996e2b9f4eb4a487b1402f74d71e6
TakingFire <38340654+TakingFire@users.noreply.github.com>
2026-07-06 17:31:35 -0700

    Add tooltip

--- src/renderer/components/Tooltip.vue
+++ src/renderer/components/Tooltip.vue
@@ -3,13 +3,23 @@
         <div class="trigger">
             <slot />
         </div>
-        <div class="content">
+        <div
+            v-if="!disabled"
+            class="content"
+        >
             <slot name="content" />
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
 
 <style scoped>
 .tooltip {
@@ -22,7 +32,7 @@
         @apply block;
     }
     & .content {
-        @apply hidden absolute w-[300px] bg-base-200 dark:bg-base-800 p-2 rounded-md z-10 shadow-2xl;
+        @apply hidden absolute w-max max-w-[300px] bg-base-200 dark:bg-base-800 p-2 rounded-md z-10 shadow-2xl;
         position-anchor: --tooltipTrigger;
         position-area: start center;
     }
